ce693492dd6f9fbb00feaf6068b755b9ccef2396
yusufaldy <aldyrek@yahoo.com>
2015-09-30 17:21:05 +0700

    Tugas Warna

---
+++ 2 MI E - 1431140067/2 MI E - 1431140067/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.Red Then
+            MyBase.BackColor = Color.Blue
+        Else : MyBase.BackColor = Color.Red
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
